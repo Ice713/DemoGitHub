ba0b432b413cfaea5c81ecfd36b79fca4daa2d4f
Ice713 <icramzilruiz@gmail.com>
2025-09-12 19:48:28 +0800

    Testing  commit

--- DemoGitHub/Form1.Designer.vb
+++ DemoGitHub/Form1.Designer.vb
@@ -24,6 +24,9 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -45,11 +48,44 @@ Partial Class Form1
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Stage, Commit, Push"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!)
+        Me.Label2.Location = New System.Drawing.Point(189, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(392, 29)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "git commit -m ""Commit Message"""
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!)
+        Me.Label3.Location = New System.Drawing.Point(189, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(510, 29)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "git config --global user.name ""UserNameKo"""
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!)
+        Me.Label4.Location = New System.Drawing.Point(189, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(574, 29)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "git config --global user.email ""account@mail.com"""
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -61,4 +97,7 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
